228e8b42aa0f9f27c452003142527dd1d9f3836b
Chris Reichert <chris.a.reichert@gmail.com>
2021-10-16 11:50:01 -0500

    completed code challenge

--- javascript/code-challenges/challenges-08.test.js
+++ javascript/code-challenges/challenges-08.test.js
@@ -118,9 +118,10 @@ Return an array containing all the matches.
 
 const isCapitalized = (str) => {
   // Solution code here...
-  let regex = /(\b[A-Z]\w+)/g;
+  // let regex = /(\b[A-Z]\w+\b)/g;
+  let regex = /([A-Z][A-Za-z]*)/g;
   let findArr = str.match(regex);
-  return findArr;
+  return findArr || [];
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -131,6 +132,14 @@ Write a function named citiesAtoJ that takes in an array of city names and uses 
 
 const citiesAtoJ = (arr) => {
   // Solution code here...
+  let localArray= [];
+  let regex = /(^[A-J])/;
+  arr.forEach((city)=>{
+    if (regex.test(city)){
+      localArray.push(city);
+    }
+  });
+  return localArray;
 };
 
 /* ------------------------------------------------------------------------------------------------
