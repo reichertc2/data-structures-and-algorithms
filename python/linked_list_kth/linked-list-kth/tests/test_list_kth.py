979e29b045946922ea374b860ed032d925cf8e71
Chris Reichert <chris.a.reichert@gmail.com>
2021-12-07 21:51:29 -0600

    finished challenge 07

--- python/linked_list_kth/linked-list-kth/tests/test_list_kth.py
+++ python/linked_list_kth/linked-list-kth/tests/test_list_kth.py
@@ -6,3 +6,55 @@ from linked_list_kth.list_kth import Node, LinkedList
 def test_code_challenge():
     assert Node
     assert LinkedList
+
+
+# @pytest.mark.skip(reason='not yet')
+def test_LinkedList_index_error():
+    linked_list = LinkedList()
+    linked_list.insert(2)
+    linked_list.insert(8)
+    linked_list.insert(3)
+    linked_list.insert(1)
+    assert  linked_list.kth_from_end(6) == IndexError
+
+
+# @pytest.mark.skip(reason='not yet')
+def test_LinkedList_a():
+    linked_list = LinkedList()
+    linked_list.insert(2)
+    linked_list.insert(8)
+    linked_list.insert(3)
+    linked_list.insert(1)
+    assert  linked_list.kth_from_end(1) == 8
+
+# @pytest.mark.skip(reason='not yet')
+def test_LinkedList_b():
+    linked_list = LinkedList()
+    linked_list.insert(2)
+    linked_list.insert(8)
+    linked_list.insert(3)
+    linked_list.insert(1)
+    assert  linked_list.kth_from_end(-1) == None
+
+# @pytest.mark.skip(reason='not yet')
+def test_LinkedList_c():
+    linked_list = LinkedList()
+    linked_list.insert(2)
+    linked_list.insert(8)
+    linked_list.insert(3)
+    linked_list.insert(1)
+    assert  linked_list.kth_from_end(3) == 1
+
+
+
+# @pytest.mark.skip(reason='not yet')
+def test_LinkedList_final():
+    linked_list = LinkedList()
+    linked_list.insert(2)
+    linked_list.insert(8)
+    linked_list.insert(3)
+    linked_list.insert(1)
+    assert  linked_list.kth_from_end(0) == 2
+    assert  linked_list.kth_from_end(1) == 8
+    assert  linked_list.kth_from_end(6) == IndexError
+
