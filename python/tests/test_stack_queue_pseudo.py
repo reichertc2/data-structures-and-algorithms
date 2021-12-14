1497f788cbd18c426e2a1cafbc9bd85ee61bd0d3
Chris Reichert <chris.a.reichert@gmail.com>
2021-12-13 22:31:55 -0600

    made some progress, not quite done

--- python/tests/test_stack_queue_pseudo.py
+++ python/tests/test_stack_queue_pseudo.py
@@ -2,6 +2,12 @@ from stack_and_queue.stack_and_queue import Node, Stack
 from code_challenges.stack_queue_pseudo.stack_queue_pseudo import PseudoQueue
 import pytest
 
+
+node_a = Node('alpha')
+node_b = Node('bravo')
+node_c = Node('charlie')
+
+
 # @pytest.mark.skip(reason='not quite time')
 def test_all_modules():
     assert Node
@@ -9,6 +15,14 @@ def test_all_modules():
     assert PseudoQueue
     assert PseudoQueue.enqueue
     assert PseudoQueue.dequeue
+
+def test_PsuedoQueue_enqueue_a():
+    pseudo_queue = PseudoQueue()
+    pseudo_queue.enqueue(node_a)
+    assert pseudo_queue.stack_in.value == 'alpha'
+    # pseudo_queue.enqueue(node_b)
+    # assert pseudo_queue.stack_in.value == 'bravo'
+    # assert pseudo_queue.stack_in.next == 'alpha'
 
 
 def test_PsuedoQueue_final():
