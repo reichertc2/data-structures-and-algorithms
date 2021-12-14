aae4de632bc37b925ad3233617e40f8247a4aa34
Chris Reichert <chris.a.reichert@gmail.com>
2021-12-14 17:03:01 -0600

    made some progress on enqueue

--- python/stack_queue_animal_shelter/stack_queue_animal_shelter.py
+++ python/stack_queue_animal_shelter/stack_queue_animal_shelter.py
@@ -1,22 +1,52 @@
-from stack_and_queue.stack_and_queue import Node,Stack
+# from stack_and_queue.stack_and_queue import Node,Stack
 
+# Create a class called AnimalShelter which holds only dogs and cats.
+# The shelter operates using a first-in, first-out approach.
 
 # isinstance() <----------
 
 class AnimalShelter:
     def __init__(self):
-        pass
+        self.front_cat = None
+        self.front_dog = None
 
-    def enqueue(self):
-        pass
+
+    def enqueue(self,value):
+        print('enqueue initial value: ',value.value)
+        if isinstance(value,Dogs):
+            self.front_dog = value
+            print('enqueue after queue assignment value: ',value.value)
+            return True
+        elif isinstance(value,Cats):
+            self.front_cat = value
+            print('enqueue after queue assignment value: ',value.value)
+            return True
+        print('if statement does not work')
+        return Exception('please add Dog or Cat')
+
 
     def dequeue(self):
         pass
 
 class Cats(AnimalShelter):
-    def __init__(self):
-        pass
+    def __init__(self,value):
+        self.value = value
 
 class Dogs(AnimalShelter):
-    def __init__(self):
-        pass
+    def __init__(self,value):
+        self.value = value
+
+
+
+
+shelter = AnimalShelter()
+dog_a = Dogs('spike')
+cat_a = Cats('fluffy')
+false_cat = 'false'
+
+shelter.enqueue(dog_a)
+shelter.enqueue(cat_a)
+
+print('dog value: ',shelter.front_dog.value)
+print('cat value: ',shelter.front_cat.value)
+
--- python/tests/test_stack_queue__animal_shelter.py
+++ python/tests/test_stack_queue__animal_shelter.py
@@ -3,6 +3,12 @@ from stack_queue_animal_shelter.stack_queue_animal_shelter import AnimalShelter,
 import pytest
 from stack_and_queue.stack_and_queue import Node,Stack
 
+shelter = AnimalShelter()
+dog_a = Dogs('spike')
+cat_a = Cats('fluffy')
+false_cat = 'false'
+
+
 def test_challenge_initialization():
     assert Node
     assert Stack
@@ -11,6 +17,37 @@ def test_challenge_initialization():
     assert AnimalShelter.dequeue
     assert Cats
     assert Dogs
+
+# @pytest.mark.skip(reason='not quite time')
+def test_AnimalShelter_enqueue_a():
+    shelter.enqueue(dog_a)
+    actual = shelter.front_dog.value
+    expected = 'spike'
+    assert actual == expected
+
+# @pytest.mark.skip(reason='not quite time')
+def test_AnimalShelter_enqueue_b():
+    shelter.enqueue(dog_a)
+    actual = shelter.front_dog.value
+    expected = True
+    assert actual == expected
+
+# @pytest.mark.skip(reason='not quite time')
+def test_AnimalShelter_enqueue_c():
+    shelter.enqueue(cat_a)
+    actual = shelter.front_cat.value
+    expected = True
+    assert actual == expected
+
+@pytest.mark.skip(reason='not quite time')
+def test_AnimalShelter_enqueue_b():
+    shelter.enqueue(false_cat)
+    actual = shelter.front.value
+    expected = Exception
+    assert actual == expected
+
+
+
 
 
 
