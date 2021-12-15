da9da2ef7d73f74badec3d63f729d983bad07db4
Chris Reichert <chris.a.reichert@gmail.com>
2021-12-15 17:27:25 -0600

    setup for challenge 13

---
+++ python/code_challenges/stack_queue_brackets/stack_queue_brackets.py
@@ -0,0 +1,8 @@
+# Write a function called validate brackets
+# Arguments: string
+# Return: boolean
+# representing whether or not the brackets in the string are balanced
+
+
+def stack_queue_brackets():
+    pass
---
+++ python/tests/test_stack_queue_brackets.py
@@ -0,0 +1,10 @@
+from code_challenges.stack_queue_brackets.stack_queue_brackets import stack_queue_brackets
+import pytest
+
+# Write a function called validate brackets
+# Arguments: string
+# Return: boolean
+# representing whether or not the brackets in the string are balanced
+@pytest.mark.skip(reason='not quite time')
+def test_stack_queue_brackets():
+    pass
