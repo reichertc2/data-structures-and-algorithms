775ca56a2e1d137b135bda98627dbee92b438194
Chris Reichert <chris.a.reichert@gmail.com>
2021-12-15 20:29:13 -0600

    finished code challenge 13

--- python/code_challenges/stack_queue_brackets/README.md
+++ python/code_challenges/stack_queue_brackets/README.md
@@ -33,5 +33,19 @@ The approach I took was to write the code first with an idea of getting to the e
 ## Solution
 <!-- Show how to run your code, and examples of it in action -->
 ```
-TBD
+regex_list = [r'[(][)]',r'[[][]]',r'[{][}]',r'[(]\w+[)]',r'[[]\w+[]]',r'[{]\w+[}]']
+
+
+def stack_queue_brackets(str):
+    valid_list = []
+    for valid_item in regex_list:
+        pattern = re.compile(valid_item)
+        if pattern.match(str):
+
+            valid_list.append(True)
+        valid_list.append(False)
+        print(valid_list)
+    if True in valid_list:
+        return True
+    return False
 ```
--- python/code_challenges/stack_queue_brackets/stack_queue_brackets.py
+++ python/code_challenges/stack_queue_brackets/stack_queue_brackets.py
@@ -2,7 +2,22 @@
 # Arguments: string
 # Return: boolean
 # representing whether or not the brackets in the string are balanced
+import re
+
+regex_list = [r'[(][)]',r'[[][]]',r'[{][}]',r'[(]\w+[)]',r'[[]\w+[]]',r'[{]\w+[}]']
 
 
-def stack_queue_brackets():
-    pass
+def stack_queue_brackets(str):
+    valid_list = []
+    for valid_item in regex_list:
+        pattern = re.compile(valid_item)
+        if pattern.match(str):
+
+            valid_list.append(True)
+        valid_list.append(False)
+        print(valid_list)
+    if True in valid_list:
+        return True
+    return False
+
+stack_queue_brackets('[]')
