dde444285cfea242962da571e61aec56c0964387
Chris Reichert <chris.a.reichert@gmail.com>
2021-12-19 19:59:56 -0600

    refactored challenge 8

--- python/code_challenges/linked_list_zip/linked_list_zip.py
+++ python/code_challenges/linked_list_zip/linked_list_zip.py
@@ -1,7 +1,24 @@
 from linked_list.list import LinkedList, Node
 
 def linked_list_zip(l_list_a,l_list_b):
-    merged_list = l_list_a
-    pull_list = l_list_b
-    print(merged_list)
-    return merged_list
+    current_a = l_list_a.head
+    current_b = l_list_b.head
+    while current_a and current_b:
+        next_a = current_a.next
+        next_b = current_b.next
+        current_a.next = current_b
+        if next_a:
+            current_b.next = next_a
+        current_a = next_a
+        current_b = next_b
+    return l_list_a if l_list_a.head else l_list_b
+
+
+l_list_a = LinkedList()
+l_list_b = LinkedList()
+l_list_a.append(Node('A'))
+l_list_a.append(Node('B'))
+l_list_a.append(Node('C'))
+
+print(l_list_a.head.value)
+# print(linked_list_zip(l_list_a, l_list_b))
