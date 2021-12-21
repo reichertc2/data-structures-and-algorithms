f105fdcf60a9cdc046192d2f41121042e02cf607
Chris Reichert <chris.a.reichert@gmail.com>
2021-12-21 14:37:08 -0600

    setup challenge 17

---
+++ python/code_challenges/tree_breadth_first/tree_breadth_first.py
@@ -0,0 +1,5 @@
+
+
+def tree_breadth_first():
+    pass
+
---
+++ python/tests/test_tree_breadth_first.py
@@ -0,0 +1,7 @@
+from code_challenges.tree_breadth_first.tree_breadth_first import tree_breadth_first
+import pytest
+
+
+@pytest.mark.skip(reason='not yet')
+def test_tree_breadth_first_final():
+    pass
