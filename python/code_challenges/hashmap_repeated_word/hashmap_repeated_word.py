9b18888e2e0a5c09fb6e08e161eaac204a745af5
Chris Reichert <chris.a.reichert@gmail.com>
2022-01-12 18:42:35 -0600

    completed string cleaning, not done

--- python/code_challenges/hashmap_repeated_word/hashmap_repeated_word.py
+++ python/code_challenges/hashmap_repeated_word/hashmap_repeated_word.py
@@ -1,2 +1,25 @@
-def hashmap_repeated_word():
-    pass
+import re
+
+def hashmap_repeated_word(string):
+    new_string = string.lower()
+    punctuation = '''!()-[]};:{'",<>\\./?@#$%^&*_~'''
+    no_punctuation = ''
+    for char in new_string:
+       if char not in punctuation:
+        no_punctuation = no_punctuation + char
+    mod_string = no_punctuation.split()
+    if len(mod_string) == 1:
+        return mod_string[0]
+
+    # for item in new_list:
+    #     if re.compile(item):
+    #         pass
+
+    # for word in new_list:
+    #     word.lower()
+
+
+
+    # create list of words in string
+    # compare lists to match first word pair
+    return mod_string
